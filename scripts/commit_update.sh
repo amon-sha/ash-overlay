a915cd66d62f6e0569f48c7e6e8b84d3a48ed58d
Amon Sha <amon.sha@gmail.com>
2024-11-06 13:15:46 +0200

    scripts/commit_update.sh: commit current dir only

--- scripts/commit_update.sh
+++ scripts/commit_update.sh
@@ -4,4 +4,5 @@ CWD=$(realpath .)
 PKG="$(basename $(dirname "$CWD"))/$(basename "$CWD")"
 VER=$(ls -t *.ebuild | head -n1 | sed 's/.*-\([0-9][^-].*\)\.ebuild$/\1/')
 MSG="Update $PKG -> $VER"
-git commit -eam "$MSG"
+git add .
+git commit -em "$MSG"
